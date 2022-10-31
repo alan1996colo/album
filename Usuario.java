4b94dbb68c3a9be5ba28026816826e0fcf5c4725
Alan <alanechabarri@gmail.com>
2022-10-31 13:36:13 -0300

    Modelado la clase usuario, ya estan todos los objetos listos para desarrollar.

---
+++ Usuario.java
@@ -0,0 +1,12 @@
+public class Usuario{
+int dni;
+String nombre;
+Album albumproio;
+public Usuario(int dni, String nombre){
+    this.dni=dni;
+    this.nombre=nombre;
+}
+boolean solicitarAlbum(String tipoAlbum){return false;}
+void comprarFigus(String tipoDeFigu){}
+
+}
